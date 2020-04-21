44be93a0c3cf3e088718922e80cf7faaeee362b6
Juan Alexis Gutierrez <juanalexisgutierrez@Juans-MBP.home>
2020-04-20 20:48:36 -0700

    done

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React from "react";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+  } from 'reactstrap';
+
+
+const Character = function(props){
+    
+console.log("props",props?.characters?.data?.results);
+const charArr = props?.characters?.data?.results;
+
+
+    return(
+        <div className="parentDiv">
+            {charArr?.map(function(e){
+                return (
+                    <div>
+                        <Card className="card">
+                            <CardImg  className="cardImg"  top width="100%" src={e.image} alt="Card image cap" />
+                            <CardBody>
+                            <div className="nameNStatus">
+                            <CardTitle>{e.name}</CardTitle>
+                            <CardSubtitle>{e.status}</CardSubtitle>
+                            </div>
+                            <CardText className="cardText">species:{e.species}<br/>gender:{e.gender}</CardText>
+                            </CardBody>
+                        </Card>
+                        </div>)
+            })}
+        </div>
+    )
+}
+
+export default Character;
